e44fee257ed057f98d72d2fce6a8438b48dd3e7f
Maria Belen Ursino <belu.ursino@icloud.com>
2024-09-26 20:44:49 -0300

    se agrega vista empleos alumno

---
+++ Alumno/Empleos-Alumno/empleos-alumno.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.container-empleo').forEach(item => {
+        item.addEventListener('click', () => {
+            const details = item.querySelector('.empleo-details');
+            details.classList.toggle('d-none'); 
+        });
+    });
+});
