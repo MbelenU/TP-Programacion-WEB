0bf5e82419af82718f4dd03a94cfc941733c761c
DrakeToT <gonzal0@live.com.ar>
2024-09-27 21:59:09 -0300

    modified Changes to be committed:     modified:   components/footer.php     modified:   components/header.php     new file:   index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include './components/header.php'; ?>
+</head>
+<body>
+    <?php include './components/navbar-admin.php'; ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
