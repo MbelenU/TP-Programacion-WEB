4269ee8eb0781b7dd44d223f599a1cadba761033
DrakeToT <gonzal0@live.com.ar>
2024-09-30 14:25:14 -0300

    modified Changes to be committed: 	modified:   Admin/Editar-Perfil/editar-perfil-admin.html 	modified:   Admin/Eventos-Admin/crear-eventoadmin.html 	modified:   Admin/Eventos-Admin/detalle-eventoadmin.html 	modified:   Admin/Eventos-Admin/editar-eventoadmin.html 	modified:   Admin/Eventos-Admin/eventos-admin.html 	modified:   Admin/Notificacion-Admin/pagina-notificaciones.html 	modified:   Admin/Perfil-Admin/perfil-admin.html 	modified:   Admin/Visualizar-Carrera/Carrera/carrera.html 	modified:   Admin/Visualizar-Carrera/visualizar-carreras-admin.html 	modified:   Admin/gestion-usuario-admin.html 	modified:   components/navbar-admin.php

--- components/navbar-admin.php
+++ components/navbar-admin.php
@@ -5,8 +5,8 @@
         <form class="d-none d-sm-flex" role="search">
             <input class="form-control me-2" type="search" placeholder="Search " aria-label="Search">
             <button class="btn btn-outline-success d-grid align-content-center" type="submit">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                    class="bi bi-search" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search"
+                    viewBox="0 0 16 16">
                     <path
                         d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
                 </svg>
@@ -26,28 +26,25 @@
             <div class="offcanvas-body">
                 <ul class="navbar-nav justify-content-start flex-grow-1 pe-3">
                     <li class="nav-item">
-                        <a href="/perfil-admin.html"><img src="/img/perfil.jpg" alt="foto-perfil"
+                        <a href="/Admin/Perfil-Admin/perfil-admin.html"><img src="/img/perfil.jpg" alt="foto-perfil"
                                 class="rounded-circle w-25"></a>
                     </li>
                     <li class="nav-item p-1">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active" aria-current="page" href="/Admin/Perfil-Admin/perfil-admin.html">Mi
+                            Perfil</a>
                     </li>
                     <li class="nav-item p-1">
-                        <a class="nav-link" href="#">Link</a>
+                        <a class="nav-link" href="/Admin/Editar-Perfil/editar-perfil-admin.html">Editar Perfil</a>
                     </li>
-                    <li class="nav-item dropdown p-1">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
+                    <li class="nav-item p-1">
+                        <a class="nav-link" href="/Admin/gestion-usuario-admin.html">Usuarios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/Admin/Eventos-Admin/eventos-admin.html">Eventos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link"
+                            href="/Admin/Notificacion-Admin/pagina-notificaciones.html">Notificaciones</a>
                     </li>
                 </ul>
                 <form class="d-flex d-sm-none mt-3" role="search">
