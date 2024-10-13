244e7fbce8750787c05868a88faf555e5161522c
DrakeToT <gonzal0@live.com.ar>
2024-10-13 01:03:09 -0300

    modified Changes to be committed: 	modified:   components/navbar-admin.php 	modified:   css/global.css

--- components/navbar-admin.php
+++ components/navbar-admin.php
@@ -1,6 +1,6 @@
 <nav class="navbar sticky-top bg-navbar">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#"><img src="<?php echo BASE_URL?>img/logo.png" alt="logo"></a>
+        <a class="navbar-brand" href="#"><img src="<?php echo BASE_URL ?>img/logo.png" alt="logo"></a>
         <!-- Buscador -->
         <form class="d-none d-sm-flex" role="search">
             <input class="form-control me-2" type="search" placeholder="Search " aria-label="Search">
@@ -15,29 +15,33 @@
         </button>
         <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar"
             aria-labelledby="offcanvasNavbarLabel">
-            <div class="offcanvas-header">
+            <div class="offcanvas-header bg-navbar">
                 <img src="<?php echo BASE_URL ?>img/logo.png" alt="logo">
                 <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
             <div class="offcanvas-body">
                 <ul class="navbar-nav justify-content-start flex-grow-1 pe-3">
                     <li class="nav-item">
-                        <a href="<?php echo BASE_URL ?>Admin/Perfil-Admin/perfil-admin.html"><img src="<?php echo BASE_URL ?>img/perfil.jpg" alt="foto-perfil"
-                                class="rounded-circle w-25"></a>
+                        <a href="<?php echo BASE_URL ?>Admin/Perfil-Admin/perfil-admin.html"><img
+                                src="<?php echo BASE_URL ?>img/perfil.jpg" alt="foto-perfil"
+                                class="rounded-circle w-25 pb-2"></a>
                     </li>
-                    <li class="nav-item p-1">
-                        <a class="nav-link active" aria-current="page" href="<?php echo BASE_URL ?>Admin/Perfil-Admin/perfil-admin.html">Mi Perfil</a>
+                    <li class="nav-item ">
+                        <a class="nav-link active" aria-current="page"
+                            href="<?php echo BASE_URL ?>Admin/Perfil-Admin/perfil-admin.html">Mi Perfil</a>
                     </li>
-                    <li class="nav-item p-1">
-                        <a class="nav-link" href="<?php echo BASE_URL ?>Admin/Editar-Perfil/editar-perfil-admin.html">Editar Perfil</a>
+                    <li class="nav-item ">
+                        <a class="nav-link"
+                            href="<?php echo BASE_URL ?>Admin/Editar-Perfil/editar-perfil-admin.html">Editar Perfil</a>
                     </li>
-                    <li class="nav-item p-1">
+                    <li class="nav-item ">
                         <a class="nav-link" href="<?php echo BASE_URL ?>Admin/gestion-usuario-admin.html">Usuarios</a>
                     </li>
-                    <li class="nav-item p-1">
-                        <a class="nav-link" href="<?php echo BASE_URL ?>Admin/Eventos-Admin/eventos-admin.html">Eventos</a>
+                    <li class="nav-item ">
+                        <a class="nav-link"
+                            href="<?php echo BASE_URL ?>Admin/Eventos-Admin/eventos-admin.html">Eventos</a>
                     </li>
-                    <li class="nav-item p-1">
+                    <li class="nav-item ">
                         <a class="nav-link"
                             href="<?php echo BASE_URL ?>Admin/Notificacion-Admin/pagina-notificaciones.html">Notificaciones</a>
                     </li>
